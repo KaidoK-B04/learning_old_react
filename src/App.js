313b09126c56668d24b609bec439b9e6b5f1e2de
KaidoK-B04 <kaido.bergen@gmail.com>
2024-02-14 14:37:20 +0100

    old version functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [likes, setLikes] = useState(20);
+  const [value, setValue] = useState("input value px");
+  const [paddingSize, setPaddingSize] = useState("20px");
+
+  function increment() {
+    setLikes(likes + 1);
+    setPaddingSize(`${parseInt(paddingSize) + 1}px`);
+  }
+  function decrement() {
+    setLikes(likes - 1);
+    setPaddingSize(`${parseInt(paddingSize) - 1}px`);
+  }
+
+  const bgColor = "green";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ backgroundColor: bgColor, padding: paddingSize, color: "white" }}
+    >
+      <h2>
+        React function for incrementing and decrementing the px size of padding
+        and text input mission.
+      </h2>
+      <p>
+        When writing input text, it will not directly been displayed in the text
+        input.
+        <br />
+        onChange calls an event where the letter you pressed is been stored in
+        value for hook in useState.
+        <br />
+        Only then React triggers to compaire the values in virtual elements and
+        User Interface. When it finds differences, it updates the text input
+        area.
+      </p>
+      <h3>{likes}</h3>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
