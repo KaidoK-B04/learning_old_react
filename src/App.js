0139ef0f99d8b0361ff9634bd073ce2860317abc
KaidoK-B04 <kaido.bergen@gmail.com>
2024-02-29 05:31:02 +0100

    Sorted and searched posts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,75 @@
-import React, { useState, useRef } from "react";
+import React, { useMemo, useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript 1", body: "Description" },
-    { id: 2, title: "Javascript 2", body: "Description" },
-    { id: 3, title: "Javascript 3", body: "Description" },
+    { id: 1, title: "aaa", body: "ccc" },
+    { id: 2, title: "bbb", body: "bbb" },
+    { id: 3, title: "ccc", body: "aaa" },
   ]);
+  const [selectedSort, setSelectedSort] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const [post, setPost] = useState({ title: "", body: "" });
+  const sortedPosts = useMemo(() => {
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      );
+    }
+    return posts;
+  }, [selectedSort, posts]);
 
-  const addNewPost = (e) => {
-    e.preventDefault();
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(searchQuery)
+    );
+  }, [searchQuery, sortedPosts]);
 
-    setPosts([...posts, { ...post, id: Date.now() }]);
-    setPost({ title: "", body: "" });
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
   };
 
   return (
     <div className="App">
-      <form>
+      <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
         <MyInput
-          value={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-          type="text"
-          placeholder="Post Title"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="search..."
         />
-
-        <MyInput
-          value={post.body}
-          onChange={(e) => setPost({ ...post, body: e.target.value })}
-          type="text"
-          placeholder="Post Description"
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="sorting"
+          options={[
+            { value: "title", name: "Titles" },
+            { value: "body", name: "Descriptions" },
+          ]}
         />
-        <MyButton onClick={addNewPost}>Make post 58</MyButton>
-      </form>
-      <PostList posts={posts} title="Posts of JS" />
+      </div>
+      {sortedAndSearchedPosts.length ? (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Posts of JS"
+        />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Posts not found</h1>
+      )}
     </div>
   );
 }
